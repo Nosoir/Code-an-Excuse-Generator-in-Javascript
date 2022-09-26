6a5f1ec142f96805eb7c01d4a48060096bdfd2b7
Nosoir <noeliairace13@gmail.com>
2022-09-26 18:23:15 +0000

    Code an Excuse Generator in Javascript

--- src/app.js
+++ src/app.js
@@ -5,7 +5,21 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
+window.onload = () => {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let who = ["the dog", "my granma", "his turtle", "my bird"];
+  let what = ["eat", "pissed", "crushed", "broked"];
+  let when = [
+    "before the class",
+    "right in time",
+    "when I finished",
+    "during my lunch",
+    "while I was praying"
+  ];
+  let randomWho = Math.floor(Math.random() * who.length);
+  let randomWhat = Math.floor(Math.random() * what.length);
+  let randomWhen = Math.floor(Math.random() * when.length);
+
+  return (document.getElementById("excuse").innerHTML =
+    who[randomWho] + " " + what[randomWhat] + " " + when[randomWhen]);
 };
